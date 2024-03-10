b7e4fe67fc9670d3207c85bcf6ceafac1bc96d68
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2024-03-10 14:19:35 +0900

    안 쓰는 변수 지움

--- src/pages/todoPage/TodoPage.jsx
+++ src/pages/todoPage/TodoPage.jsx
@@ -1,12 +1,12 @@
-import {useQuery} from "@tanstack/react-query";
-import {TodoApi} from "../../api/TodoApi";
+import { useQuery } from "@tanstack/react-query";
+import { TodoApi } from "../../api/TodoApi";
 import TodoListItem from "./components/TodoListItem";
-import classes from './TodoPage.module.scss';
+import classes from "./TodoPage.module.scss";
 
 function TodoPage() {
-  const { isLoading, error, data, isFetching } = useQuery({
-    queryKey: ['fetchAllTodoList'],
-    queryFn: async ()=> {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["fetchAllTodoList"],
+    queryFn: async () => {
       const res = await TodoApi.getAllTodoList();
       return res.data;
     },
@@ -17,11 +17,12 @@ function TodoPage() {
   if (error) return "An error has occurred: " + error.message;
 
   return (
-      <div className={classes.pagewrapper}>
-        <h1>오늘 할 일</h1>
-        {
-        data.map((data, idx)=><TodoListItem key={idx} item={data}/> )
-      }</div>
+    <div className={classes.pagewrapper}>
+      <h1>오늘 할 일</h1>
+      {data.map((data, idx) => (
+        <TodoListItem key={idx} item={data} />
+      ))}
+    </div>
   );
 }
 
--- src/pages/todoPage/components/TodoEditModal.jsx
+++ src/pages/todoPage/components/TodoEditModal.jsx
@@ -1,130 +1,135 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./TodoEditModal.module.scss";
-import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
-import {TodoApi} from "../../../api/TodoApi";
-import {UiStore} from "../../../store/UiStore";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { TodoApi } from "../../../api/TodoApi";
+import { UiStore } from "../../../store/UiStore";
 
-const TodoEditModal = ({id}) => {
-    const modalClose = UiStore(state => state.actions.modalClose);
+const TodoEditModal = ({ id }) => {
+  const modalClose = UiStore((state) => state.actions.modalClose);
 
-    const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-    const [inputValue, setInputValue] = useState({
+  const [inputValue, setInputValue] = useState({
+    id: id,
+    userId: 0,
+    title: "",
+    completed: false,
+  });
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["fetchTodoItem", id],
+    queryFn: async () => {
+      const res = await TodoApi.getTodoItem(id);
+      return res.data;
+    },
+  });
+
+  const updateData = useMutation({
+    mutationFn: (params) => TodoApi.updateTodoItem(params),
+    onSuccess: () => {
+      // Mutation 에 성공한 뒤 실행됨
+      queryClient.invalidateQueries({
+        queryKey: ["fetchAllTodoList"], // fetchAllTodoList 의 데이터를 무효화 시킴.
+      });
+      alert("수정되었습니다!");
+      modalClose();
+    },
+    onError: () => {
+      // mutation이 Error를 만나면 실행됨, try-catch-finally의 catch 역할임
+      alert("error");
+    },
+    onSettled: () => {
+      // mutation이 성공해서 성공한 데이터 또는 Error가 전달될 때 try-catch-finally의 finally 역할임
+    },
+    onMutate: () => {
+      // mutationFn이 실행되기 전에 실행되고, mutation 함수가 받을 동일한 변수가 전달된다.
+      // optimistic update 사용 시 유용한 함수이다. (백앤드에 API를 요청하고 나서, 기다리지 않고 response를 업데이트 하는 것)
+    },
+  });
+  /*
+   * updateData.mutate : updateData.mutate() => mutationFn을 실행한다.
+   * updateData.mutateAsync : .mutate와 같으나 promise를 반환한다.
+   *
+   * */
+
+  const handleUpdateClick = async () => {
+    updateData.mutate({
+      id: id,
+      userId: data.userId,
+      title: inputValue.title,
+      completed: inputValue.completed,
+    });
+  };
+
+  const handleCloseClick = () => {
+    modalClose();
+  };
+
+  useEffect(() => {
+    if (data) {
+      setInputValue({
         id: id,
-        userId: 0,
-        title: '',
-        completed: false,
-    });
+        userId: data.userId,
+        title: data.title,
+        completed: data.completed,
+      });
+    }
+  }, [data, id]);
 
-    const {isLoading, isSuccess, error, data, isFetching} = useQuery({
-        queryKey: ["fetchTodoItem",id],
-        queryFn: async () => {
-            const res = await TodoApi.getTodoItem(id);
-            return res.data;
-        },
-        cacheTime: 0,
-    });
+  if (isLoading) return "Loading...";
 
-    const updateData = useMutation({
-        mutationFn: (params) => TodoApi.updateTodoItem(params),
-        onSuccess: () => {
-            // Mutation 에 성공한 뒤 실행됨
-            queryClient.invalidateQueries({
-                queryKey: ['fetchAllTodoList'] // fetchAllTodoList 의 데이터를 무효화 시킴.
-            });
-            modalClose();
-        },
-        onError: () => {
-            // mutation이 Error를 만나면 실행됨, try-catch-finally의 catch 역할임
-        },
-        onSettled: () => {
-            // mutation이 성공해서 성공한 데이터 또는 Error가 전달될 때 try-catch-finally의 finally 역할임
-        },
-        onMutate: () => {
-            // mutationFn이 실행되기 전에 실행되고, mutation 함수가 받을 동일한 변수가 전달된다.
-            // optimistic update 사용 시 유용한 함수이다. (백앤드에 API를 요청하고 나서, 기다리지 않고 response를 업데이트 하는 것)
-        }
-    });
-    /*
-    * updateData.mutate : updateData.mutate() => mutationFn을 실행한다.
-    * updateData.mutateAsync : .mutate와 같으나 promise를 반환한다.
-    *
-    * */
+  if (error) return "An error has occurred: " + error.message;
 
-    const handleUpdateClick = async () => {
-        updateData.mutate({
-            id: id,
-            userId: data.userId,
-            title: inputValue.title,
-            completed: inputValue.completed,
-        });
-    };
-
-    const handleCloseClick = () => {
-        modalClose();
-    };
-
-    useEffect(() => {
-        if (data) {
-            setInputValue({
-                id: id,
-                userId: data.userId,
-                title: data.title,
-                completed: data.completed,
-            })
-        }
-    }, [data]);
-
-    if (isLoading) return "Loading...";
-
-    if (error) return "An error has occurred: " + error.message;
-
-    return (
-        <div className={classes.wrapper}>
-            <div className={classes.inputWrapper}>
-                <label htmlFor="id">id : </label>
-                <input
-                    id="id"
-                    value={id}
-                    onChange={() => {
-                    }}
-                    disabled
-                    className={classes.input}
-                />
-            </div>
-            <div className={classes.inputWrapper}>
-                <label htmlFor="title">일정명 : </label>
-                <input
-                    id="title"
-                    value={inputValue.title}
-                    onChange={(e) => {
-                        setInputValue(prev => ({
-                            ...prev,
-                            title: e.target.value,
-                        }))
-                    }}
-                    className={classes.input}/>
-            </div>
-            <div className={classes.inputWrapper}>
-                <label htmlFor={'completed'}>완료여부 : </label>
-                <input
-                    id="completed"
-                    type="checkbox"
-                    checked={inputValue.completed}
-                    onChange={(e) => {
-                        console.log(e.target.checked)
-                        setInputValue(prev => ({
-                            ...prev,
-                            completed: e.target.checked
-                        }))
-                    }}
-                    className={classes.checkbox}
-                />
-            </div>
-            <button className={classes.button} onClick={handleUpdateClick}>수정하기</button>
-            <button className={classes.button} onClick={handleCloseClick}>닫기</button>
-        </div>
-    );
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.inputWrapper}>
+        <label htmlFor="id">id : </label>
+        <input
+          id="id"
+          value={id}
+          onChange={() => {}}
+          disabled
+          className={classes.input}
+        />
+      </div>
+      <div className={classes.inputWrapper}>
+        <label htmlFor="title">일정명 : </label>
+        <input
+          id="title"
+          value={inputValue.title}
+          onChange={(e) => {
+            setInputValue((prev) => ({
+              ...prev,
+              title: e.target.value,
+            }));
+          }}
+          className={classes.input}
+        />
+      </div>
+      <div className={classes.inputWrapper}>
+        <label htmlFor={"completed"}>완료여부 : </label>
+        <input
+          id="completed"
+          type="checkbox"
+          checked={inputValue.completed}
+          onChange={(e) => {
+            console.log(e.target.checked);
+            setInputValue((prev) => ({
+              ...prev,
+              completed: e.target.checked,
+            }));
+          }}
+          className={classes.checkbox}
+        />
+      </div>
+      <button className={classes.button} onClick={handleUpdateClick}>
+        수정하기
+      </button>
+      <button className={classes.button} onClick={handleCloseClick}>
+        닫기
+      </button>
+    </div>
+  );
 };
 
 export default TodoEditModal;
